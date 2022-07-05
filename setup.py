5b6d8527d4015531a9499fc58053760e57cc4eab
thelocomotion <thelocomotionnn@gmail.com>
2022-07-05 05:50:52 +0200

    More arranges

--- setup.py
+++ setup.py
@@ -2,17 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='GloriousProtonManager',
-    version='0.0.11',
+    version='0.1',
+    description='Manage GE-Proton',
     license='GNU General Public License v3 (GPLv3)',
     author="Loco Motion",
     author_email='thelocomotionnn@gmail.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/thelocomotion/GloriousProtonManager',
-    keywords='example project',
+    keywords='proton proton-ge wine linux valve',
     install_requires=[
           'PySimpleGUI',
-          'requests',
-          'urllib3',
       ],
 )
