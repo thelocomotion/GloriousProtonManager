4a072829abf83fa516184d4fc6c942781f8e0009
thelocomotion <thelocomotionnn@gmail.com>
2022-07-06 02:41:52 +0200

    Adding dirs to gitignore according to python gitignore template

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='GloriousProtonManager',
-    version='0.24',
+    version='0.25',
     python_requires='>=3.10',
     description='Manage GE-Proton with a simple UI',
     license='GNU General Public License v3 (GPLv3)',
